036c5d7ee0b10c12f28b38f0d50f1fc12669c803
lukslowak <lukslowak@gmail.com>
2018-08-25 17:05:29 +0200

    commit3

--- src/main/java/com/example/videos/HomeController.java
+++ src/main/java/com/example/videos/HomeController.java
@@ -1,21 +1,37 @@
 package com.example.videos;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.lang.model.element.Name;
 
 //1.
 @Controller
-    public class HomeController {
-        //2
-        @GetMapping ("/")
-        public String hello(){
-            return "hello";}
+public class HomeController {
+    //2
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
 
-            //3.zrob html
-            //4.registry > compiler.automake.allow.when.app.runningd
-         @GetMapping("/video")
-          public String video (){
-             return "video";
-            }
-        }
+    //3.zrob html
+    //4.registry > compiler.automake.allow.when.app.runningd
+    @GetMapping("/video")
+    public String video() {
+        return "video";
+    }
 
+
+    @GetMapping("/videoPlay")
+    public String add(@RequestParam String name,
+                      @RequestParam String url,
+                      ModelMap map) {
+        VideoName videoName = new VideoName();
+        videoName.setName(name);
+        videoName.setUrl(url);
+        map.put("videoName", videoName);
+        return "videoPlay";
+    }
+}
---
+++ src/main/java/com/example/videos/VideoName.java
@@ -0,0 +1,30 @@
+package com.example.videos;
+
+public class VideoName {
+
+    private String name;
+
+    public VideoName(){}
+
+    public VideoName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
